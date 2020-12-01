9e921a0f00298181ec9c2f778c052a2ac4007a11
Jesus PC <tutorialesnetbeans@gmail.com>
2020-12-01 16:40:01 -0500

    Intro

---
+++ Consumir/src/components/Consumir.vue
@@ -0,0 +1,31 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            usuarios:[],
+            parametro:{
+                numid:'',
+                nombre:'',
+                apellido:'',
+                mail:''
+            }
+        }
+    },
+    created(){
+
+        this.mostrar();
+    },
+    methods:{
+        const respuesta=this.mostrar
+        this.parametro.numid=100;
+        const response=await axios.get("http://localhost/apibd/api/api.php?op=2&numid="+this.parametro.numid)
+    }
+}
+</script>
+<style>
+
+</style>
